580e6529b1fb152027ac86507aee40a1c2a6802c
asom <86374869+as0mdude@users.noreply.github.com>
2023-08-22 17:35:51 -0700

    button

--- src/App.js
+++ src/App.js
@@ -9,11 +9,10 @@ function App() {
       
       <div class="maintext"><h1>Vincent Fu.</h1>
       <h3>CS Major @ UC Santa Cruz.</h3></div>
-      <a href = "vincentfu_csresume2023.pdf" download="vincentfu_csresume2023.pdf">
-      <button type="button">
-            Download Resume
-       </button>
-         </a>
+      
+      
+       <a href="https://files.catbox.moe/bzx633.pdf" class="button">Download Resume</a>
+   
        
       
       
