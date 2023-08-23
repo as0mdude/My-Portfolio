ac08b30353b44404332b48050566dc569c3033cf
asom <86374869+as0mdude@users.noreply.github.com>
2023-08-22 17:14:59 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  
+      return(
+      <>
+      
+      
+      <div class="maintext"><h1>Vincent Fu.</h1>
+      <h3>CS Major @ UC Santa Cruz.</h3></div>
+      <a href = "vincentfu_csresume2023.pdf" download="vincentfu_csresume2023.pdf">
+      <button type="button">
+            Download Resume
+       </button>
+         </a>
+       
+      
+      
+      
+      
+      </>
+      );
+    };
+
+
+export default App;
