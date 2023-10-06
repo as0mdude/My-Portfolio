213e3529920640eedbf73f1588ef7226d919c456
asom <86374869+as0mdude@users.noreply.github.com>
2023-10-05 22:55:52 -0700

    updated resume

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
       
       
       <div class="maintext"><h1>Vincent Fu.</h1>
-      <h3>CS Major @ UC Santa Cruz.</h3></div>
+      <h3>CS Major @ UC Santa Cruz.</h3><a href="https://files.catbox.moe/pj5q9e.pdf" class="button">Download Resume</a></div>
       
       
-       <a href="https://files.catbox.moe/bzx633.pdf" class="button">Download Resume</a>
+       
    
        
       
