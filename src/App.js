c4f8d0ee473ae2f9d785ffb8de94597fa597c10e
asom <86374869+as0mdude@users.noreply.github.com>
2023-10-23 14:49:38 -0700

    visibility

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <>
       
       
-      <div class="maintext"><h1>Vincent Fu.</h1>
-      <h3>CS Major @ UC Santa Cruz.</h3><a href="https://files.catbox.moe/pj5q9e.pdf" class="button">Download Resume</a></div>
+      <div class="maintext"><h1>Vincent Fu</h1>
+      <h3>CS Major @ UC Santa Cruz</h3><a href="https://files.catbox.moe/pj5q9e.pdf" class="button">Download Resume</a></div>
       
       
        
